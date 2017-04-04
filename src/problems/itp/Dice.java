65a89324ef10f17ffbf144193192bcfa6aa70486
srtk86 <immunetocommonsense@gmail.com>
2017-04-04 22:21:29 +0900

    add leftEar method to Dice class

--- src/problems/itp/Dice.java
+++ src/problems/itp/Dice.java
@@ -49,4 +49,39 @@ public class Dice {
   public int eye() {
     return this.t;
   }
+
+  public int leftEar(int head, int face) {
+    if (head == this.t) {
+      if (face == this.s) return this.e;
+      if (face == this.e) return this.n;
+      if (face == this.n) return this.w;
+      if (face == this.w) return this.s;
+    } else if (head == this.s) {
+      if (face == this.t) return this.w;
+      if (face == this.w) return this.b;
+      if (face == this.b) return this.e;
+      if (face == this.e) return this.t;
+    } else if (head == this.n) {
+      if (face == this.t) return this.e;
+      if (face == this.e) return this.b;
+      if (face == this.b) return this.w;
+      if (face == this.w) return this.t;
+    } else if (head == this.e) {
+      if (face == this.t) return this.s;
+      if (face == this.s) return this.b;
+      if (face == this.b) return this.n;
+      if (face == this.n) return this.t;
+    } else if (head == this.w) {
+      if (face == this.t) return this.n;
+      if (face == this.n) return this.b;
+      if (face == this.b) return this.s;
+      if (face == this.s) return this.t;
+    } else if (head == this.b) {
+      if (face == this.n) return this.e;
+      if (face == this.e) return this.s;
+      if (face == this.s) return this.w;
+      if (face == this.w) return this.n;
+    }
+    return 0;
+  }
 }
