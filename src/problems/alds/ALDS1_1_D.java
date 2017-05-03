bea6cd6efcc81571c8b1be6c74f5dc31e74466fa
srtk86 <immunetocommonsense@gmail.com>
2017-05-03 16:23:54 +0900

    ALDS1_1_D

--- src/problems/alds/ALDS1_1_D.java
+++ src/problems/alds/ALDS1_1_D.java
@@ -1,5 +1,26 @@
 package problems.alds;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ALDS1_1_D {
 
+  public static void main(String[] args) {
+
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(in.readLine());
+      int max = Integer.MIN_VALUE;
+      int min = Integer.MAX_VALUE;
+      for (int i = 0; i < n; i++) {
+        int r = Integer.parseInt(in.readLine());
+        max = Math.max(max, r - min);
+        min = Math.min(min, r);
+      }
+      System.out.println(max);
+    } catch (NumberFormatException | IOException e) {
+      // ignore
+    }
+  }
 }
