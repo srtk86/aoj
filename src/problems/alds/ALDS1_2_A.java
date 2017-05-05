d0a47a6786f4a66a723ddfcdea464e7081798c94
srtk86 <immunetocommonsense@gmail.com>
2017-05-05 19:55:40 +0900

    ALDS1_2_A

--- src/problems/alds/ALDS1_2_A.java
+++ src/problems/alds/ALDS1_2_A.java
@@ -1,5 +1,41 @@
 package problems.alds;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
 public class ALDS1_2_A {
 
+  public static void main(String[] args) {
+
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(in.readLine());
+      String[] ss = in.readLine().split(" ");
+      int[] nums = Stream.of(ss).mapToInt(Integer::parseInt).toArray();
+
+      int cnt = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = n - 1; j > i; j--) {
+          if (nums[j] < nums[j - 1]) {
+            cnt++;
+            int tmp = nums[j];
+            nums[j] = nums[j - 1];
+            nums[j - 1] = tmp;
+          }
+        }
+      }
+      
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < n; i++) {
+        if (i != 0) sb.append(" ");
+        sb.append(nums[i]);
+      }
+      System.out.println(sb.toString());
+      System.out.println(cnt);
+    } catch (NumberFormatException | IOException e) {
+      // ignore
+    }
+  }
 }
